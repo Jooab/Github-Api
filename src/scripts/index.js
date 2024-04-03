a9f706d54cafc5df251325b47cc54c8076f4920d
Joab <joobascanal@gmail.com>
2024-04-03 20:18:04 -0300

    segunda tentantiva de mudança do caminho da pasra dos arquivos importados

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
-import { getRepositories } from "../scripts/services/repositories.js"
-import { getUser } from "../scripts/services/user.js"
+import { getRepositories } from "./services/repositories.js"
+import { getUser } from "./services/user.js"
 
-import { user } from "../scripts/objects/user.js"
-import { screen } from "../scripts/objects/screen.js"
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
 
 document.getElementById('btn-search').addEventListener('click', ()=>{
     const userName = document.getElementById('input-search').value
